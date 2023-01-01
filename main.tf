d46b7da99b60238a62780a50afb22e0c447ef739
Artur Sirota <59325398+artsirgenx@users.noreply.github.com>
2023-01-01 13:45:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "web" {
   instance_type = "t3.nano"
 
   tags = {
-    name    = "HelloWorld"
-    creator = "artur.sirota@netapp.com"
+    Name    = "HelloWorld"
+    Creator = "artur.sirota@netapp.com"
   }
 }
